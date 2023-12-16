2ac412a5dc0df8b847da815d3b3e9f2f1efb1327
Didier Prophete <didier.prophete@gmail.com>
2023-12-15 23:46:16 -0800

    n/s/e/w

--- day14/p.exs
+++ day14/p.exs
@@ -14,11 +14,11 @@ defmodule P1 do
     end
   end
 
-  def move_left(c, ?#), do: {c, ?#}
-  def move_left(?., c), do: {c, ?.}
-  def move_left(c1, c2), do: {c1, c2}
+  def _move_west(c, ?#), do: {c, ?#}
+  def _move_west(?., c), do: {c, ?.}
+  def _move_west(c1, c2), do: {c1, c2}
 
-  def move_left(row) do
+  def _move_west(row) do
     # .oo..#o..o.. => oo...#o.o...
     # oo...#o.o... => oo...#oo....
     row1 = row ++ [?.]
@@ -26,7 +26,7 @@ defmodule P1 do
     {[_ | new_row], _} =
       row1
       |> Enum.reduce({[], ?#}, fn c, {acc, previous} ->
-        {c1, c2} = move_left(previous, c)
+        {c1, c2} = _move_west(previous, c)
         {acc ++ [c1], c2}
       end)
 
@@ -36,20 +36,48 @@ defmodule P1 do
   def move_north(platform) do
     platform
     |> transpose()
-    |> Enum.map(&move_left/1)
+    |> Enum.map(&_move_west/1)
     |> transpose()
   end
 
-  def full_move_north(platform) do
-    new_platform = move_north(platform)
+  def move_west(platform) do
+    platform
+    |> Enum.map(&_move_west/1)
+  end
+
+  def move_south(platform) do
+    platform
+    |> Enum.reverse()
+    |> transpose()
+    |> Enum.map(&_move_west/1)
+    |> transpose()
+    |> Enum.reverse()
+  end
+
+  def move_east(platform) do
+    platform
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.map(&_move_west/1)
+    |> Enum.map(&Enum.reverse/1)
+  end
+
+  def full_move(platform, func) do
+    new_platform = func.(platform)
 
     case new_platform == platform do
       true -> new_platform
-      false -> full_move_north(new_platform)
+      false -> full_move(new_platform, func)
     end
   end
 
-  def pp(platform) do
+  def full_move_north(platform), do: full_move(platform, &move_north/1)
+  def full_move_west(platform), do: full_move(platform, &move_west/1)
+  def full_move_south(platform), do: full_move(platform, &move_south/1)
+  def full_move_east(platform), do: full_move(platform, &move_east/1)
+
+  def pp(platform, txt) do
+    IO.puts("========= #{txt}")
+
     platform
     |> Enum.map(&List.to_string/1)
     |> Enum.each(&IO.puts/1)
@@ -70,14 +98,57 @@ defmodule P1 do
     platform = parse_file(filename)
 
     platform
+    |> pp("original")
     |> full_move_north()
-    |> pp()
+    |> pp("after full move north")
     |> weigh()
     |> IO.inspect(label: "total")
   end
 end
 
 defmodule P2 do
+  @cache %{}
+
+  def tilt(platform) do
+    if Map.has_key?(@cache, platform) do
+      IO.puts("!!!!!!! cache hit")
+      Map.get(@cache, platform)
+    else
+      new_platform =
+        platform
+        |> P1.full_move_north()
+        |> P1.full_move_west()
+        |> P1.full_move_south()
+        |> P1.full_move_east()
+
+      Map.put(@cache, platform, new_platform)
+      new_platform
+    end
+  end
+
+  def run(filename) do
+    platform = P1.parse_file(filename)
+
+    platform
+    |> P1.pp("original")
+    |> tilt()
+    |> tilt()
+    |> tilt()
+    |> P1.pp("after 3 tilt")
+    |> P1.weigh()
+    |> IO.inspect(label: "total")
+
+    0..10_000
+    |> Enum.reduce_while(platform, fn i, platform ->
+      new_platform = tilt(platform)
+
+      case new_platform == platform do
+        true -> {:halt, {:ok, platform, i}}
+        false -> {:cont, new_platform}
+      end
+    end)
+    |> IO.inspect(label: "after 10_000 tilt")
+  end
 end
 
 # P1.run("sample.txt")
