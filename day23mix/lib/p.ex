a03fe7922a49e425d13f38e358c4e60caa0b4ce3
Didier Prophete <didier.prophete@gmail.com>
2023-12-26 08:51:47 -0800

    day 23 - part 2 - works but takes 2 hours

--- day23mix/lib/p.ex
+++ day23mix/lib/p.ex
@@ -112,9 +112,72 @@ defmodule P1 do
 end
 
 defmodule P2 do
+  # --------------------------------------------------------------------------------
+  # - running
+  # --------------------------------------------------------------------------------
+
+  def at(map, {x, y}) do
+    case Enum.at(map, y) do
+      nil ->
+        :wall
+
+      row ->
+        case Enum.at(row, x) do
+          ?# -> :wall
+          ?. -> :empty
+        end
+    end
+  end
+
+  def nx_moves(map, {x, y}, visited) do
+    [
+      {x, y - 1},
+      {x, y + 1},
+      {x - 1, y},
+      {x + 1, y}
+    ]
+    |> Enum.filter(fn p -> !(p in visited) && at(map, p) == :empty end)
+  end
+
+  def walk_one(map, exit, max_so_far, to_process, {current, visited}, count) do
+    process_next = fn potential_max, count ->
+      max_so_far = max(max_so_far, potential_max)
+
+      case to_process do
+        [] ->
+          # nothing else to process -> we are done
+          max_so_far
+
+        [first | rest] ->
+          walk_one(map, exit, max_so_far, rest, first, count)
+      end
+    end
+
+    if current == exit do
+      # we reached then exit !
+      IO.inspect(
+        "[DDA] max #{max_so_far}, found exit ##{count + 1} with length #{Enum.count(visited)}"
+      )
+
+      process_next.(Enum.count(visited), count + 1)
+    else
+      case nx_moves(map, current, visited) do
+        [] ->
+          # no more moves -> we are done with this path
+          process_next.(-1, count)
+
+        nxs ->
+          visited = MapSet.put(visited, current)
+          [first | rest] = nxs |> Enum.map(&{&1, visited})
+          walk_one(map, exit, max_so_far, rest ++ to_process, first, count)
+      end
+    end
+  end
+
   def run(filename) do
     {map, entrance, exit} = P1.parse_file(filename)
 
+    # cleanup map
     map =
       for row <- map do
         for c <- row do
@@ -128,14 +191,32 @@ defmodule P2 do
         end
       end
 
-    paths = P1.walk_one(map, exit, [entrance])
+    walk_one(map, exit, 0, [], {entrance, MapSet.new()}, 0)
+    |> IO.inspect(label: "[DDA] max")
 
     # for path <- paths do
     #   IO.puts("\n== path length #{Enum.count(path) - 1}")
     #   pp_map_with_path(map, path)
     # end
 
-    paths |> Enum.map(&(Enum.count(&1) - 1)) |> Enum.max() |> IO.inspect()
+    # paths |> Enum.map(&(Enum.count(&1) - 1)) |> Enum.max() |> IO.inspect()
+
+    # prepare for dijkstra_dfs
+    #   graph =
+    #     for {row, y} <- Enum.with_index(map), {c, x} <- Enum.with_index(row) do
+    #       case c do
+    #         ?. ->
+    #           case P1.nx_moves(map, [{x, y}]) do
+    #             [] -> nil
+    #             neighboors -> {{x, y}, Enum.map(neighboors, &{&1, -1})}
+    #           end
+
+    #         _ ->
+    #           nil
+    #       end
+    #     end
+    #     |> Enum.filter(&(&1 != nil))
+    #     |> Enum.into(%{})
   end
 end
 
@@ -143,8 +224,8 @@ defmodule P do
   def start() do
     Cache.setup()
     # P1.run("sample.txt")
-    P1.run("input.txt")
-    # P2.run("sample.txt")
+    # P1.run("input.txt")
+    P2.run("sample.txt")
     # P2.run("input.txt")
   end
 end
