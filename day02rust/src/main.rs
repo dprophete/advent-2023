bb7b109a18491932d1f0eb401304c99c03236010
Didier Prophete <didier.prophete@gmail.com>
2024-06-02 20:13:24 -0700

    using split_once

--- day02rust/src/main.rs
+++ day02rust/src/main.rs
@@ -5,19 +5,19 @@ use std::{cmp::max, fs};
 //--------------------------------------------------------------------------------
 
 fn process_line(line: &str) -> bool {
-    let (_, parts) = line.split_once(": ").unwrap();
-    let reaches: Vec<_> = parts.split("; ").collect();
-    for reach in reaches {
-        let cubes: Vec<_> = reach.split(", ").collect();
+    let (_, turns) = line.split_once(": ").unwrap();
+    let turns: Vec<_> = turns.split("; ").collect();
+    for turn in turns {
+        let cubes = turn.split(", ").collect::<Vec<_>>();
         for cube in cubes {
             let (mut blue, mut red, mut green) = (0, 0, 0);
-            let cube_parts: Vec<_> = cube.split(" ").collect();
-            let num = cube_parts[0].parse().unwrap();
-            match cube_parts[1] {
-                "blue" => blue = num,
-                "red" => red = num,
-                "green" => green = num,
-                _ => (),
+            let (amount, color) = cube.split_once(" ").unwrap();
+            let amount: i32 = amount.parse().unwrap();
+            match color {
+                "blue" => blue = amount,
+                "red" => red = amount,
+                "green" => green = amount,
+                _ => panic!("unexpected color"),
             }
             if red > 12 || green > 13 || blue > 14 {
                 return false;
@@ -43,19 +43,19 @@ fn p1(input: &str) {
 //--------------------------------------------------------------------------------
 
 fn process_line2(line: &str) -> i32 {
-    let (_, parts) = line.split_once(": ").unwrap();
-    let reaches: Vec<_> = parts.split("; ").collect();
+    let (_, turns) = line.split_once(": ").unwrap();
+    let turns: Vec<_> = turns.split("; ").collect();
     let (mut blue, mut red, mut green) = (0, 0, 0);
-    for reach in reaches {
-        let cubes: Vec<_> = reach.split(", ").collect();
+    for turn in turns {
+        let cubes = turn.split(", ").collect::<Vec<_>>();
         for cube in cubes {
-            let cube_parts: Vec<_> = cube.split(" ").collect();
-            let num = cube_parts[0].parse().unwrap();
-            match cube_parts[1] {
-                "blue" => blue = max(blue, num),
-                "red" => red = max(red, num),
-                "green" => green = max(green, num),
-                _ => (),
+            let (amount, color) = cube.split_once(" ").unwrap();
+            let amount: i32 = amount.parse().unwrap();
+            match color {
+                "blue" => blue = max(blue, amount),
+                "red" => red = max(red, amount),
+                "green" => green = max(green, amount),
+                _ => panic!("unexpected color"),
             }
         }
     }
